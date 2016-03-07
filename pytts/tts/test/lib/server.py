38d52aa3254c9aa73d023e31c35c2f104bde4f69
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2016-03-07 17:30:21 +0100

    Revert "Revert "Removed code redundancies in Tests""
    This reverts commit e364e90b9556e615add1d82c3ff83c8ed14e2658.

--- pytts/tts/test/lib/server.py
+++ pytts/tts/test/lib/server.py
@@ -2,7 +2,10 @@
 Base for Server Tests
 """
 
+from http.client import HTTPConnection
+from json import loads
 from time import sleep
+from unittest import TestCase
 
 from ...control.manager import ControlManager
 from ...util.config import ConfigurationFileFinder
@@ -36,3 +39,63 @@ def cleanup(cls_instance):
     sleep(5)
     SingletonMeta.delete(ConfigurationFileFinder)
     SingletonMeta.delete(ControlManager)
+
+
+class ServerTestCase(TestCase):
+    """
+    Base Class for a Server Test
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Setup the server class for login test
+        """
+        cls.config = dict()
+        cls.ctrl_manager = None
+        prepare_test(cls)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Shutdown server and clean up
+        """
+        cleanup(cls)
+
+    def util_evaluate_json_response(self, response):
+        """
+        Evaluate the response for JSON
+
+        :param response: The response to evaluate
+        """
+        self.assertEqual(200, response.code)
+        self.assertIn('Content-Type', response.headers)
+        self.assertEqual('application/json', response.headers['Content-Type'])
+
+    def get_http_connection(self):
+        """
+        Get a HTTP connection with the exisiting credentials
+
+        :return: A http connection
+        :rtype: HTTPConnection
+        """
+        return HTTPConnection(
+            host=self.config['bind_ip'],
+            port=self.config['bind_port']
+        )
+
+    def get_json_response(self, url: str):
+        """
+        Get a JSON response
+
+        :param url: The URL to get
+        :return: The JSON respnse
+        """
+        connection = self.get_http_connection()
+        connection.request(url=url, method='GET')
+        response = connection.getresponse()
+        self.util_evaluate_json_response(response)
+        data = response.read()
+        json_data = loads(data.decode(encoding='utf-8'))
+        connection.close()
+        return json_data
--- pytts/tts/test/unit/test_login_api.py
+++ pytts/tts/test/unit/test_login_api.py
@@ -2,47 +2,17 @@
 Test the Login API
 """
 
-from json import loads
-from http.client import HTTPConnection
-from unittest import TestCase
-
-from ..lib.server import prepare_test, cleanup
+from ..lib.server import ServerTestCase
 
 
-class LoginAPITest(TestCase):
+class LoginAPITest(ServerTestCase):
     """
     Check simple Version Call
     """
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        Setup the server class for login test
-        """
-        cls.config = dict()
-        prepare_test(cls)
-
-    @classmethod
-    def tearDownClass(cls):
-        """
-        Shutdown server and clean up
-        """
-        cleanup(cls)
 
     def test_status_call(self):
         """
         Test if there is a login answer
         """
-        connection = HTTPConnection(
-            host=self.config['bind_ip'],
-            port=self.config['bind_port']
-        )
-        connection.request(url='/api/v1.0/login/status', method='GET')
-        response = connection.getresponse()
-        self.assertEqual(200, response.code)
-        self.assertIn('Content-Type', response.headers)
-        self.assertEqual('application/json', response.headers['Content-Type'])
-        data = response.read()
-        json_data = loads(data.decode(encoding='utf-8'))
-        connection.close()
+        json_data = self.get_json_response('/api/v1.0/login/status')
         self.assertDictEqual({'status': -1}, json_data)
