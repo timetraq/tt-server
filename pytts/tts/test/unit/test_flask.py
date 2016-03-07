e364e90b9556e615add1d82c3ff83c8ed14e2658
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2016-03-07 17:26:11 +0100

    Revert "Removed code redundancies in Tests"
    This reverts commit d6249bedcdbaf8229477fe2c6cb93570f9c5a73a.

--- pytts/tts/test/unit/test_flask.py
+++ pytts/tts/test/unit/test_flask.py
@@ -2,17 +2,47 @@
 Test basic flask
 """
 
-from ..lib.server import ServerTestCase
+from json import loads
+from http.client import HTTPConnection
+from unittest import TestCase
+
+from ..lib.server import prepare_test, cleanup
 
 
-class BasicFlaskResponseTest(ServerTestCase):
+class BasicFlaskResponseTest(TestCase):
     """
     Check simple Version Call
     """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Setup the server class
+        """
+        cls.config = dict()
+        prepare_test(cls)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Shutdown stuff
+        """
+        cleanup(cls)
 
     def test_version_call(self):
         """
         Test if there is a version answer
         """
-        json_data = self.get_json_response('/api/version')
+        connection = HTTPConnection(
+            host=self.config['bind_ip'],
+            port=self.config['bind_port']
+        )
+        connection.request(url='/api/version', method='GET')
+        response = connection.getresponse()
+        self.assertEqual(200, response.code)
+        self.assertIn('Content-Type', response.headers)
+        self.assertEqual('application/json', response.headers['Content-Type'])
+        data = response.read()
+        json_data = loads(data.decode(encoding='utf-8'))
+        connection.close()
         self.assertDictEqual({'version': '1.0'}, json_data)
