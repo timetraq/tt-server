1d0f3c5154cabeaff033f467d4f5771871126f0e
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2016-03-25 19:46:01 +0100

    Patched version of BootstrapDialog

--- pytts/tts/app/static/tt-app/tt.js
+++ pytts/tts/app/static/tt-app/tt.js
@@ -3,5 +3,17 @@
 (function (namespace, $) {
     'use strict';
 
+    var local = {};
+
+    local.registrationDialog = function () {
+        BootstrapDialog.show({
+            title: 'Registration',
+            message: $('<div></div>').load('dialogs/registration.xhtml')
+        });
+    };
+
+    $(window).ready(function () {
+        local.registrationDialog();
+    });
 
 })(window.tts = window.tts || {}, jQuery);
